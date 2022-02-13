82ff5729a5f1698a0cdc16409cb97403641c96b0
Alvin Yanson <yansonalvin29@gmail.com>
2022-02-13 18:16:47 +0800

    fonts

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
-import Home from "./screens/home";
-import { StyleSheet } from "react-native";
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import React, { useState } from 'react';
+import Home from './screens/home';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <Home />
   );
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/home.js
+++ screens/home.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-
-
 export default function Home() {
-    return (
-        <View style={styles.container}>
-            <Text>Home Screen</Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Home Screen</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 24
-    }
-})
+  container: {
+    padding: 24,
+  },
+  titleText: {
+    fontFamily: 'nunito-bold',
+    fontSize: 30,
+  }
+});
